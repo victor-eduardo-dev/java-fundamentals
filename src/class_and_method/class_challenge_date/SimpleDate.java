b94f0fb1bc484e7e9123b185e3dc62fa3f98c175
Victor Eduardo Rosa <eduvictor.rosa@gmail.com>
2026-06-22 17:36:27 -0300

    Add simple date class challenge

--- src/class_and_method/class_challenge_date/SimpleDate.java
+++ src/class_and_method/class_challenge_date/SimpleDate.java
@@ -1,31 +1,44 @@
 package class_and_method.class_challenge_date;
 
-public class Date {
+public class SimpleDate {
     int day;
     int month;
     int year;
 
-    Date(int day, int month, int year) {
+    SimpleDate(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
 
+    private String zeroLeft(int value) {
+        // %02d formata números inteiros com no mínimo 2 dígitos.
+        // Se o número tiver apenas 1 dígito, um zero é adicionado à esquerda.
+        // Exemplos:
+        // 1 -> 01
+        // 5 -> 05
+        return String.format("%02d", value);
+    }
+
     public String dateFormattedToBr() {
-        String formattedDay;
-        String formattedMonth;
+        return String.format("%s/%s/%d", zeroLeft(this.day), zeroLeft(this.month), this.year);
+    }
 
-        if (this.day < 10) {
-            formattedDay = String.format("0%d", this.day);
-        } else {
-            formattedDay = String.format("%d", this.day);
+    private String monthName() {
+        String[] months = {
+                "Janeiro", "Fevereiro", "Março", "Abril",
+                "Maio", "Junho", "Julho", "Agosto",
+                "Setembro", "Outubro", "Novembro", "Dezembro"
+        };
+
+        if (this.month < 1 || this.month > 12) {
+            return "Mês inválido";
         }
 
-        if (this.month < 10) {
-            formattedMonth = String.format("0%d", this.month);
-        } else {
-            formattedMonth = String.format("%d", this.month);
-        }
-        return String.format("%s/%s/%s", formattedDay, formattedMonth, this.year);
+        return months[this.month - 1];
+    }
+
+    public String dateFormattedToBrWithMonthName() {
+        return String.format("%s de %s de %d", zeroLeft(this.day), monthName(), this.year);
     }
 }
--- src/class_and_method/class_challenge_date/SimpleDateMain.java
+++ src/class_and_method/class_challenge_date/SimpleDateMain.java
@@ -1,4 +1,27 @@
 package class_and_method.class_challenge_date;
 
-public class DateMain {
+import util.ConsoleFormatter;
+import util.Divider;
+
+public class SimpleDateMain {
+
+    public static final int SEP_COUNT = 42;
+
+    public static void main(String[] args) {
+        //Exibe o título.
+        ConsoleFormatter.showInstructions("Exibir e Formatar Data", SEP_COUNT);
+
+        // Cria um novo objeto SimpleDate.
+        SimpleDate date = new SimpleDate(1, 12, 2023);
+
+        // Chama os métodos de formatação de datas.
+        String shortDateFormat = date.dateFormattedToBr();
+        String fullDateFormat = date.dateFormattedToBrWithMonthName();
+
+        // Exibe os dois formatos para a data.
+        System.out.printf("Data comum: %s%n", shortDateFormat);
+        Divider.separator(SEP_COUNT);
+        System.out.printf("Data por extenso: %s%n", fullDateFormat);
+        Divider.separator(SEP_COUNT);
+    }
 }
